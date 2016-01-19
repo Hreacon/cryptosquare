493627e52c44ef9de60dab51fb9ab3c3ccbd1886
Nicholas Jensen-Hay <nicholasjensenhay@gmail.com>
2016-01-19 14:27:16 -0800

    add spec for convertString. fails.

---
+++ spec/specs.js
@@ -0,0 +1,5 @@
+describe('cryptosquare', function() {
+  it("is thisisfifteenbehaviortest when the input is this, is, fifteen behavior test!!!", function() {
+    expect(convertString("this, is, fifteen behavior test!!!")).to.equal("thisisfifteenbehaviortest");
+  });
+});
