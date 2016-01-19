018d871094e1c6efc6378996905dcff0ab819e41
Nicholas Jensen-Hay <nicholasjensenhay@gmail.com>
2016-01-19 14:35:00 -0800

    add spec for straight string to square

--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,7 @@ describe('cryptosquare', function() {
   it("is thisisfifteenbehaviortest when the input is this, is, fifteen behavior test!!!", function() {
     expect(convertString("this, is, fifteen behavior test!!!")).to.equal("thisisfifteenbehaviortest");
   });
+  it("is thisi sfift teenb ehavi ortes t when input is thisisfifteenbehaviortest", function() {
+    expect(squareStraightString("thisisfifteenbehaviortest")).to.equal("thisi sfift teenb ehavi ortes t");
+  });
 });
