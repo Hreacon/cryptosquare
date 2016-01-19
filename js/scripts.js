203481ab8d1dcf60bc45f3621d63e85cabaef41f
Nicholas Jensen-Hay <nicholasjensenhay@gmail.com>
2016-01-19 15:56:30 -0800

    make it work. logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,47 @@
 var convertString = function(regularSentance) {
   return regularSentance.replace(/[^\w]/g, '');
 }
-
+var splitStringByLength = function(string, length) {
+  return string.match(new RegExp('.{1,' + length + '}', 'g')).toString();
+}
 var squareStraightString = function(straightString) {
   // length of string
   // get as close to the square root as possible
   var columns = Math.round(Math.sqrt(straightString.length));
+  console.log("Columns: " + columns);
   var rows = Math.round(straightString.length/columns);
+  console.log("Rows: " + rows);
   var square = [];
   var sIndex = 0;
-
-  return straightString.match(new RegExp('.{1,' + columns + '}', 'g')).toString();
+  var output = splitStringByLength(straightString, columns);
+  console.log("squareStraightString output: " + output);
+  return output;
 }
 
 var cryptSquare = function(square) {
-  
+  // get 5 letter 'words' going top to bottom down the square
+  var five = [];
+  square = square.split(',');
+  console.log("Columns: " + square.length);
+  var upperBarrier = square.join('').length/5 -1;
+  console.log("cryptSquare, " + upperBarrier);
+  for(var up = 0;up<upperBarrier;up++) {
+    console.log("Iteration " + up);
+    for(var index = 0;index<square.length;index++) {
+      console.log("Inside Iteration " + index + " " + square[index%square.length]);
+      var squa = square[index%square.length][up];
+      if(squa !== undefined) five = five + squa;
+    }
+    five = five + '';
+  }
+  return splitStringByLength(five, 5);
 }
 
 $(document).ready(function() {
-  // r$("body").text(squareStraightString(convertString("this, is, fifteen behavior test!!!")));
+  $("form").submit(function(event){
+    var regularSentance = $("#cryptoInput").val();
+    $(".output").text(cryptSquare(squareStraightString(convertString(regularSentance))));
+    event.preventDefault();
+  });
+  //
 });
--- spec/specs.js
+++ spec/specs.js
@@ -2,10 +2,10 @@ describe('cryptosquare', function() {
   it("is thisisfifteenbehaviortest when the input is this, is, fifteen behavior test!!!", function() {
     expect(convertString("this, is, fifteen behavior test!!!")).to.equal("thisisfifteenbehaviortest");
   });
-  it("is thisi sfift teenb ehavi ortes t when input is thisisfifteenbehaviortest", function() {
+  it("is thisi,sfift,eenbe,havio,rtest when input is thisisfifteenbehaviortest", function() {
     expect(squareStraightString("thisisfifteenbehaviortest")).to.equal("thisi,sfift,eenbe,havio,rtest");
   });
-  it("is tsteo thfeh riiea tsfnv eifbi s when input is thisi,sfift,eenbe,havio,rtest", function() {
-    expect(cryptSquare("thisi,sfift,eenbe,havio,rtest")).to.equal("tsteo thfeh riiea tsfnv eifbi s");
+  it("is tsehr hfeat iinve sfbis iteot when input is thisi,sfift,eenbe,havio,rtest", function() {
+    expect(cryptSquare("thisi,sfift,eenbe,havio,rtest")).to.equal("tsehr hfeat iinve sfbis iteot ");
   });
 });
